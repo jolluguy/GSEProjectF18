d7cc4d4cda0a07c5a159acad42b9181de9d62281
Rasmus <Rasmus@192.168.10.117>
2018-04-27 17:58:24 +0200

    har lavet dataclasser for CPR Citizen og Inquiry og derres tilhærende interfaces

---
+++ src/Acquaintance/IInquiry.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acquaintance;
+
+/**
+ *
+ * @author Rasmus
+ */
+public interface IInquiry {
+public ICPR getCpr();
+public void setCpr(ICPR cpr);
+public ICitizen getCitizen();
+public void setCitizen(ICitizen citizen);
+public String getProblemDescription();
+public void setProblemDescription(String problemDescription);
+}
---
+++ src/DataPersistens/SaveInquiryCSV.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataPersistens;
+
+import Acquaintance.IInquiry;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Rasmus
+ */
+public class SaveInquiryCSV {
+
+    IInquiry inq;
+
+    SaveInquiryCSV(DataInquiry inq) {
+        this.inq = new DataInquiry(new DataCPR(inq.getCpr().getCprNumber()), new DataCitizen(inq.getCitizen().getFirstname(), inq.getCitizen().getSurname(), inq.getCitizen().getRoadName(), inq.getCitizen().getHouseNumber(), inq.getCitizen().getFloor(), inq.getCitizen().getPostNumber(), inq.getCitizen().getCity(), inq.getCitizen().getCity()), inq.getProblemDescription());
+    saveInq();
+    
+    }
+    
+    void saveInq(){
+        final String SEPPERATOR = "<";
+        
+        try (FileWriter fw = new FileWriter("InquiryDB.txt", true); // true betyder at den appender filen i stedet for at overrite den
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter writer = new PrintWriter(bw)) {
+            writer.println(
+                    inq.getCpr().getCprNumber()+
+                    SEPPERATOR+
+                    inq.getCitizen().getFirstname()+
+                    SEPPERATOR+
+                    inq.getCitizen().getSurname()+
+                    SEPPERATOR+
+                    inq.getCitizen().getRoadName()+
+                    SEPPERATOR+
+                    inq.getCitizen().getHouseNumber()+
+                    SEPPERATOR+
+                    inq.getCitizen().getFloor()+
+                    SEPPERATOR+
+                    inq.getCitizen().getPostNumber()+
+                    SEPPERATOR+
+                    inq.getCitizen().getCity()+
+                    SEPPERATOR+
+                    inq.getCitizen().getCity()+
+                    SEPPERATOR+
+                    inq.getProblemDescription());
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+}
