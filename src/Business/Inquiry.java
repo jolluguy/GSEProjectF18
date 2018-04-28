877b504457b2a8bf46643f7dc324f2f1eb0dfdca
jolluguy <32259239+jolluguy@users.noreply.github.com>
2018-04-28 11:53:51 +0200

    få ændringer

--- src/Business/Inquiry.java
+++ src/Business/Inquiry.java
@@ -13,28 +13,10 @@ import Acquaintance.IInquiry;
  */
 public class Inquiry implements IInquiry{
     
-    private int cprNumber;
     private String problemDescription;
-    private String firstname;
-    private String surname;
-    private String roadName;
-    private String houseNumber;
-    private String floor;
-    private int postNumber;
-    private String city;
-    private String tlfNumber;
     
     Inquiry (int cprNumber, String problemDescription, String firstname, String surname, String roadName, String houseNumber, String floor, int postNumber, String city, String tlfNumber) {
-        this.cprNumber = cprNumber;
         this.problemDescription = problemDescription;
-        this.firstname = firstname;
-        this.surname = surname;
-        this.roadName = roadName;
-        this.houseNumber = houseNumber;
-        this.floor = floor;
-        this.postNumber = postNumber;
-        this.city = city;
-        this.tlfNumber = tlfNumber;
         
         Citizen citizen = new Citizen(cprNumber, firstname, surname, roadName, houseNumber, floor, postNumber, city, tlfNumber);
     }
