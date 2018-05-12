bc5fe5dcac3db9d6936eda3a914e02c86960d6f4
CasaRol <alexander-rol@hotmail.com>
2018-05-12 20:45:57 +0200

    small changes

--- src/LoginPersistence/DatabaseManager.java
+++ src/LoginPersistence/DatabaseManager.java
@@ -2,6 +2,7 @@ package LoginPersistence;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,48 +12,38 @@ import java.util.logging.Logger;
 public class DatabaseManager {
 
     Connection conn;
-    String url = "postgres://ksevsemh:S_ACh-Y8moY8kuw4BtFMiGDWoqLrzc_o@horton.elephantsql.com:5432/ksevsemh";
+    String url = "jdbc:postgresql://horton.elephantsql.com:5432/ksevsemh";
     String username = "ksevsemh";
     String password = "S_ACh-Y8moY8kuw4BtFMiGDWoqLrzc_o";
 
-    public final Connection dbConnection() {
+    
 
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.getMessage();
-        }
-
-        try {
-            DriverManager.getConnection(url, username, password);
-
-        } catch (SQLException ex) {
-            Logger.getLogger(DatabaseManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return conn;
-    }
-
-    public void createUserInDB(String username, String password, boolean b, java.util.Date createdTime, java.util.Date lastLoginTime) {
-        System.out.println("createUserInDB entered");
+    public boolean createUserInDB(String username, String password, boolean b, java.util.Date createdTime, java.util.Date lastLoginTime) {
         try {
             Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager.getConnection(url, username, password);
-            Statement st = conn.createStatement();
+            PreparedStatement st = conn.prepareStatement("insert into login (brugernavn, kodeord, status, created_time, last_login_time) "
+                    + "values (" + username + ", " + password + ", " + b + ", " + createdTime + ", " + lastLoginTime + ");");
 
-            
-            
-            String sql = "INSERT INTO login(brugernavn, kodeord, status, created_time, last_login_time) "
-                    + "VALUES(" + username + ", " + password + ", " + true + ", "
-                    + createdTime + ", " + lastLoginTime + ");";
-            
-            st.executeUpdate(sql);
-            
+//            String sql = "INSERT INTO login(brugernavn, kodeord, status, created_time, last_login_time) "
+//                    + "VALUES(" + username + ", " + password + ", " + true + ", "
+//                    + createdTime + ", " + lastLoginTime + ");";
+
+            st.setString(1, username);
+            st.setString(2, password);
+            st.setBoolean(3, true);
+            st.setObject(4, createdTime);
+            st.setObject(5, lastLoginTime);
+
+            st.executeUpdate();
+
             System.out.println("Query successfull");
-            
-            
+
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
+
+        return true;
 
     }
 
