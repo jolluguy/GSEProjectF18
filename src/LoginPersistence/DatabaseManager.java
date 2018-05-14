0640479e415f65d1a51c1c6b6274a569d98590a1
CasaRol <alexander-rol@hotmail.com>
2018-05-14 10:16:54 +0200

    Small changes

--- src/LoginPersistence/DatabaseManager.java
+++ src/LoginPersistence/DatabaseManager.java
@@ -5,10 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class DatabaseManager {
 
